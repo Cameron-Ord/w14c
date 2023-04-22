51178cff732200234eafb9f1ea905272a4acfb4b
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-22 15:48:44 -0600

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,108 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import LoginPage from '@/views/LoginPage.vue'
+import GamePage from '@/views/GamePage.vue'
+import PageHeader from '@/views/PageHeader.vue'
+import PageFooter from '@/views/PageFooter.vue'
 Vue.use(VueRouter)
+
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+
+    path: `/`,
+
+    component: LoginPage,
+
+    meta: [
+      {
+
+        title: `Login-Page`
+
+      },
+      {
+
+        name: `description`,
+
+        content: `welcome to the login page`
+
+      },
+      {
+
+        name: `author`,
+
+        content: `Cameron Ord`
+      },
+    ]
+
+
   },
+
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+
+    path: `/game`,
+
+    component: GamePage
+  },
+
+  {
+
+    path: `/`,
+
+    component: PageFooter
+  },
+
+  {
+
+    path: `/`,
+
+    component: PageHeader
   }
+
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+
+  let current_meta_tags = document.querySelectorAll(`meta`);
+
+  for(let i = 0; i<current_meta_tags.length; i++){
+
+    current_meta_tags[i].remove();
+  }
+
+  let new_meta_tags = to[`meta`];
+
+  document.querySelector(`title`)[`innerText`] = new_meta_tags[0][`title`];
+
+  for(let i = 0; i<new_meta_tags.length; i++){
+
+    document.querySelector(`head`).insertAdjacentHTML(`beforeend`,
+    
+    `<meta name="${new_meta_tags[i][`name`]}"
+    
+    content="${new_meta_tags[i][`content`]}">`);
+
+    document.querySelector(`head`).insertAdjacentHTML(`afterbegin`,
+    
+    `<meta charset="utf-8">
+    
+    <meta http-equiv=""X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">`
+    
+    );
+  }
+
+  from;
+
+  next();
+
+}
+);
+
 
 export default router
