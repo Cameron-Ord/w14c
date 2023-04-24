b613940b69662e206c253f997d087f6e3d517457
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-04-23 19:40:40 -0600

    fixed meta tags

--- src/router/index.js
+++ src/router/index.js
@@ -112,16 +112,18 @@ router.beforeEach((to, from, next) => {
     
     content="${new_meta_tags[i][`content`]}">`);
 
-    document.querySelector(`head`).insertAdjacentHTML(`afterbegin`,
-    
-    `<meta charset="utf-8">
-    
-    <meta http-equiv=""X-UA-Compatible" content="IE=edge">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">`
-    
-    );
+
   }
+
+  document.querySelector(`head`).insertAdjacentHTML(`afterbegin`,
+    
+  `<meta charset="utf-8">
+  
+  <meta http-equiv=""X-UA-Compatible" content="IE=edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">`
+  
+  );
 
   from;
 
